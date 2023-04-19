16d7fe611f32fdde1149a70562ee565088ca28ba
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-19 23:49:05 +0300

    ju

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import ProvideMainServer from "@caphub-funding/mainserver-provider";
+import { Typography } from "@mui/material";
+
 function App() {
-  return <p>oc to be...</p>;
+  return (
+    <>
+      <Typography>Main Server:</Typography>
+      <ProvideMainServer>
+        <Typography>Main Server is Alive </Typography>
+      </ProvideMainServer>
+    </>
+  );
 }
 
 export default App;
